82ac41bf9d3b6627d6075beab020ee4380a44819
Mohammadjan1122 <63337310+Mohammadjan1122@users.noreply.github.com>
2020-10-24 09:21:53 +0430

    Add files via upload
    Telegram chanel  https://t.me/sultani1122

---
+++ termux_tools_pkg.sh
@@ -0,0 +1,77 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by Mohammad Hacker"
+echo ""
+echo ""
+echo ""
+echo "subscribe  our channel"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "░█████╗░  ███╗░░░███╗
+██╔══██╗  ████╗░████║
+███████║  ██╔████╔██║
+██╔══██║  ██║╚██╔╝██║
+██║░░██║  ██║░╚═╝░██║
+╚═╝░░╚═╝  ╚═╝░░░░░╚═╝
+
+
+
+██╗░░██╗░█████╗░░█████╗░██╗░░██╗███████╗██████╗░
+██║░░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+███████║███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝
+██╔══██║██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║
+╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝"
+echo " telegram chanel  : @sultani1122
+youtube chanel mohammad hacker   
+github ; https://github.com/mohammadjan1122"
+echo ""
+echo " this sceipt craeted by mohammad sultani 👌😌❤🙋‍♂️   Subscribe our Youtube channel and press Bell icon"
+exit
